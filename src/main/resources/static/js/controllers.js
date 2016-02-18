7e2eb93db40bd41f92bf37f6dd2ad68a1c805d77
Florian Thienel <florian@thienel.org>
2016-02-18 20:15:08 +0100

    allow to reset the alarm

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,29 +1,27 @@
 (function() {
 	'use strict';
 	var controllers = angular.module('idaControllers', ['ngSanitize']);
-	
+
 	controllers.controller('DashboardController', ['$scope', '$window', '$location', 'ida.remote', 'ida.alarm', function($scope, $window, $location, remote, alarm) {
 		$scope.$on('ida.reloadDashboards', function(event) {
 			$window.location.reload();
 		});
-		
-		$scope.$on('ida.alarm', function(event, telegram) {
+
+		$scope.$on('ida.alarm.trigger', function(event, telegram) {
 			$location.path("/alarm");
 		});
 	}]);
-	
+
 	controllers.controller('InformationDashboardController', ['$scope', '$interval', 'ida.state', function($scope, $interval, state) {
-		var PAGE_FLIP_DELAY = 5000;
+		var PAGE_FLIP_DELAY = 10000;
 		var nextPageFlip;
-		
+
 		$scope.information = [];
 		$scope.currentPage = 0;
 		$scope.pageCount = 1;
 		$scope.pageSize = 7;
-		
-		$scope.$on('state.information', function(event, information) {
-			$scope.stopPageFlip();
-			
+
+		function setInformation(information) {
 			if (information === undefined || information.length === 0) {
 				$scope.information = [];
 			} else {
@@ -31,72 +29,92 @@
 			}
 			$scope.currentPage = 0;
 			$scope.pageCount = Math.ceil($scope.information.length / $scope.pageSize);
-			
+		}
+
+		$scope.$on('state.information', function(event, information) {
+			$scope.stopPageFlip();
+			setInformation(information);
 			$scope.startPageFlip();
 		});
-		
+
 		$scope.flipToNextPage = function() {
 			$scope.currentPage = ($scope.currentPage + 1) % $scope.pageCount;
 		};
-		
+
 		$scope.startPageFlip = function() {
 			if ($scope.pageCount <= 1) return;
-			
+
 			nextPageFlip = $interval($scope.flipToNextPage, PAGE_FLIP_DELAY);
 			console.log('Page flipping started');
 		};
-		
+
 		$scope.stopPageFlip = function() {
 			if (nextPageFlip === undefined) return;
-			
+
 			$interval.cancel(nextPageFlip);
 			nextPageFlip = undefined;
 			console.log('Page flipping stopped');
 		};
-		
+
 		$scope.$on('destroy', function() {
 			$scope.stopPageFlip();
 		});
+
+		setInformation(state.getInformation());
 	}]);
-	
+
 	controllers.controller('RoomScheduleController', ['$scope', 'ida.state', function($scope, state) {
 		$scope.rooms = [];
-		
-		$scope.$on('state.rooms', function(event, rooms) {
+
+		function setRooms(rooms) {
 			if (rooms === undefined || rooms.length === 0) {
 				$scope.rooms = [];
 			} else {
 				$scope.rooms = rooms;
 			}
+		}
+
+		$scope.$on('state.rooms', function(event, rooms) {
+			setRooms(rooms);
 		});
+
+		setRooms(state.getRooms());
 	}]);
-	
-	controllers.controller('AlarmTelegramController', ['$scope', 'ida.alarm', function($scope, alarm) {
+
+	controllers.controller('AlarmTelegramController', ['$scope', '$location', 'ida.alarm', function($scope, $location, alarm) {
 		var telegram = alarm.current();
 		$scope.keyword = telegram.keyword;
 		$scope.address = telegram.address;
 		$scope.additionalInformation = telegram.additionalInformation;
-		
+
 		if (telegram.lat === undefined || telegram.lon === undefined) {
 			telegram.lat = 49.826302;
 			telegram.lon = 10.735984;
 		}
-		
+
 		var map = L.map('map').setView([telegram.lat, telegram.lon], 17);
 		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		    maxZoom: 18
+			maxZoom: 18
 		}).addTo(map);
 		L.tileLayer('http://openfiremap.org/hytiles/{z}/{x}/{y}.png', {
 			maxZoom: 18
 		}).addTo(map);
 		var marker = L.marker([telegram.lat, telegram.lon]).addTo(map);
+
+		$scope.$on('ida.alarm.trigger', function(event, telegram) {
+			// TODO update view based on telegram
+		});
+		$scope.$on('ida.alarm.reset', function(event) {
+			$location.path("/dashboard");
+		});
+
 	}]);
-	
+
 	controllers.controller('AdminController', ['$scope', 'ida.state', 'ida.remote', 'ida.alarm', function($scope, state, remote, alarm) {
 		$scope.information = "";
 		$scope.room1 = "";
 		$scope.room2 = "";
-		
+
 		$scope.publishInformation = function() {
 			var lines = $scope.information.trim().split('\n');
 			var information = [];
@@ -106,7 +124,7 @@
 
 				var info;
 				if (line.startsWith('!')) {
-					info = {text: line.substr(1), label: '!'}
+					info = {text: line.substr(1), label: '!'};
 				} else {
 					info = {text: line};
 				}
@@ -114,7 +132,7 @@
 			}
 			state.setInformation(information);
 		};
-		
+
 		$scope.publishRoomSchedule = function() {
 			var rooms = [];
 			if ($scope.room1 !== '') {
@@ -124,14 +142,18 @@
 				rooms.push({name: "Schulung 2", text: $scope.room2});
 			}
 			state.setRooms(rooms);
-		}
-		
+		};
+
 		$scope.reloadDashboards = function() {
 			remote.reloadDashboards();
-		}
-		
+		};
+
 		$scope.triggerAlarm = function() {
 			alarm.triggerAlarm({lat: 49.826302, lon: 10.735984, keyword: "B4 Person", address: "Steigerwaldstraße 13, Burgebrach", additionalInformation: ""});
-		}
+		};
+
+		$scope.resetAlarm = function() {
+			alarm.resetAlarm();
+		};
 	}]);
 })();
--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -1,86 +1,105 @@
 (function() {
 	'use strict';
 	var services = angular.module('idaServices', []);
-	
-	services.factory('ida.state', ['$rootScope', 'ida.mqtt', function($rootScope, mqtt) {
-		handle('/ida/information', 'state.information');
-		handle('/ida/rooms', 'state.rooms');
 
-		function handle(destinationPath, name) {
+	services.factory('ida.state', ['$rootScope', 'ida.mqtt', function($rootScope, mqtt) {
+		var information = [];
+		var rooms = [];
+
+		handle('/ida/information', 'state.information', function(payload) { information = payload; });
+		handle('/ida/rooms', 'state.rooms', function(payload) { rooms = payload; });
+
+		function handle(destinationPath, name, callback) {
 			mqtt.subscribe(destinationPath);
 			$rootScope.$on(destinationPath, function(event, message) {
-				$rootScope.$broadcast(name, JSON.parse(message.payloadString));
-			})
+				var payload = JSON.parse(message.payloadString);
+				callback(payload);
+				$rootScope.$broadcast(name, payload);
+			});
 		}
 
 		function set(name, object) {
 			mqtt.send(name, JSON.stringify(object));
 		}
-		
+
 		function setRetained(name, object) {
 			mqtt.sendRetained(name, JSON.stringify(object));
 		}
-		
+
 		return {
 			setInformation: function(information) {
 				setRetained('/ida/information', information);
 			},
+			getInformation: function() {
+				return information;
+			},
 			setRooms: function(rooms) {
 				setRetained('/ida/rooms', rooms);
 			},
+			getRooms: function() {
+				return rooms;
+			}
 		};
 	}]);
-	
+
 	services.factory('ida.remote', ['$rootScope', 'ida.mqtt', function($rootScope, mqtt) {
 		handle('/ida/remote/reloadDashboards', 'ida.reloadDashboards');
-		
+
 		function handle(destinationPath, name) {
 			mqtt.subscribe(destinationPath);
 			$rootScope.$on(destinationPath, function(event, message) {
 				$rootScope.$broadcast(name, JSON.parse(message.payloadString));
 			});
 		}
-		
+
 		return {
 			reloadDashboards: function() {
-				mqtt.send('/ida/remote/reloadDashboards', 'now ' + Date.now());
+				mqtt.send('/ida/remote/reloadDashboards', JSON.stringify(Date.now()));
 			}
 		};
 	}]);
-	
+
 	services.factory('ida.alarm', ['$rootScope', 'ida.mqtt', function($rootScope, mqtt) {
 		var alarmTelegram = {};
-		
-		mqtt.subscribe('/ida/alarm');
-		$rootScope.$on('/ida/alarm', function(event, message) {
-			alarmTelegram = JSON.parse(message.payloadString);
-			$rootScope.$broadcast('ida.alarm', alarmTelegram);
-		});
-		
+		handle('/ida/alarm/trigger', 'ida.alarm.trigger', function(payload) { alarmTelegram = payload; });
+		handle('/ida/alarm/reset', 'ida.alarm.reset', function() { alarmTelegram = {}; });
+
+		function handle(destinationPath, name, callback) {
+			mqtt.subscribe(destinationPath);
+			$rootScope.$on(destinationPath, function(event, message) {
+				var payload = JSON.parse(message.payloadString);
+				callback(payload);
+				$rootScope.$broadcast(name, payload);
+			});
+		}
+
 		return {
 			current: function() {
 				return alarmTelegram;
 			},
 			triggerAlarm: function(telegram) {
-				mqtt.send('/ida/alarm', JSON.stringify(telegram));
+				mqtt.send('/ida/alarm/trigger', JSON.stringify(telegram));
+			},
+			resetAlarm: function() {
+				mqtt.send('/ida/alarm/reset', JSON.stringify(Date.now()));
 			}
-		}
+		};
 	}]);
-	
+
 	services.factory('ida.mqtt', ['$rootScope', '$location', '$http', function($rootScope, $location, $http) {
 		var connected = false;
 		var pendingSubscriptions = [];
 		var pahoClient;
-		
+
 		function connect(hostname, port, path) {
 			console.log("Connecting to MQTT broker on " + hostname + " port " + port + " path " + path);
 			var client = new Paho.MQTT.Client(hostname, Number(port), path, "ida" + Date.now());
-			
+
 			client.onMessageArrived = function(message) {
 				console.log("onMessageArrived " + message.destinationName);
 				$rootScope.$broadcast(message.destinationName, message);
-			}
-			
+			};
+
 			client.onConnectionLost = function(responseObject) {
 				if (responseObject !== 0) {
 					console.log("onConnectionLost: " + responseObject.errorMessage);
@@ -90,23 +109,23 @@
 
 			client.connect({
 				onSuccess: function() {
-					console.log("Connected to MQTT broker.")
+					console.log("Connected to MQTT broker.");
 					connected = true;
 					applyPendingSubscriptions();
-				}, 
+				},
 				onFailure: function() {
 					console.log("Cannot connect to MQTT broker.");
 					connected = false;
 				}
 			});
-			
+
 			return client;
 		}
 
 		function addPendingSubscription(filter, options) {
 			pendingSubscriptions.push({filter: filter, options: options});
 		}
-		
+
 		function applyPendingSubscriptions() {
 			for (var i in pendingSubscriptions) {
 				var pendingSubscription = pendingSubscriptions[i];
@@ -116,7 +135,7 @@
 			pendingSubscriptions = [];
 		}
 
-		
+
 		$http({
 			method: 'GET',
 			url: '/mqtt.conf'
@@ -126,14 +145,14 @@
 				console.log("Found mqtt.conf");
 				console.log(response.data);
 				pahoClient = connect(response.data.hostname, response.data.port, response.data.path);
-			}, 
+			},
 			function onError(response) {
-				console.log("Cannot find mqtt.conf, using defaults.")
+				console.log("Cannot find mqtt.conf, using defaults.");
 				console.log(response);
 				pahoClient = connect($location.host(), 18830, "/iotDashboard");
 			}
 		);
-		
+
 		return {
 			subscribe: function(filter, options) {
 				if (!connected) {
@@ -141,7 +160,7 @@
 					addPendingSubscription(filter, options);
 					return;
 				}
-				
+
 				console.log("Subscribing to " + filter);
 				pahoClient.subscribe(filter, options);
 			},
@@ -150,7 +169,7 @@
 					console.log("Not connected, cannot unsubscribe from " + filter);
 					return;
 				}
-				
+
 				console.log("Unsubscribing from " + filter);
 				pahoClient.unsubscribe(filter, options);
 			},
@@ -159,7 +178,7 @@
 					console.log("Not connected, cannot send to " + destinationName);
 					return;
 				}
-				
+
 				var message = new Paho.MQTT.Message(payload);
 				message.destinationName = destinationName;
 				pahoClient.send(message);
@@ -169,7 +188,7 @@
 					console.log("Not connected, cannot send to " + destinationName);
 					return;
 				}
-				
+
 				var message = new Paho.MQTT.Message(payload);
 				message.destinationName = destinationName;
 				message.retained = true;
